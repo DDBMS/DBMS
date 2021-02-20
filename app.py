e77eb4814c488c5905585847cac2d6d6b86cf850
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-02-20 16:35:35 +0800

    Update

--- app.py
+++ app.py
@@ -52,6 +52,46 @@ def upload():
         'data': encoded
     })
 
+@app.route('/file/content', methods=['POST'])
+def content():
+    """
+    data
+    tag
+    key
+    """
+
+    tag = request.form.get('tag')
+    key = request.form.get('key')
+    data = request.files['data']
+    encoded = base64.b64encode(data.read()).decode('utf-8')
+
+    db_settings = {
+        "host": "172.50.0.3",
+        "port": 3306,
+        "user": "root",
+        "password": "root_toor",
+        "db": "my_db",
+        "charset": "utf8"
+    }
+
+    try:
+        # 建立Connection物件
+        conn = pymysql.connect(**db_settings)
+
+        cursorObject = conn.cursor()
+        sqlQuery = "CREATE TABLE Employee(id int, LastName varchar(32), FirstName varchar(32), DepartmentCode int)"
+        # cursorObject.execute(sqlQuery)
+
+    except Exception as ex:
+        print(ex)
+
+    print(encoded)
+    return jsonify({
+        'status': True,
+        'tag': tag,
+        'data': encoded
+    })
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=10022)
